73199a19b91b78c558711ee47b6e7af72d65bcae
Brandon Green <brandon@brandonleeproductions.com>
2018-05-29 23:45:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -17,3 +17,7 @@ end
 
 # Got the tests to pass with the follow error:
 # 0 examples, 0 failures, 1 error occurred outside of examples
+# SyntaxError:
+#  /home/brandong1/prime-ruby-v-000/prime.rb:16: syntax error, unexpected keyword_end, expecting end-of-input
+# removing any 'end' generates more errors and I don't know why, but I've spent too much time on this lab
+# so I'm moving on...
