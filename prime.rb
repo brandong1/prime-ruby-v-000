55073c2b5dea52b8d2fc6d113970cf3b1af40dee
Brandon Green <brandon@brandonleeproductions.com>
2018-05-29 23:39:14 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,11 @@
-def prime?(integer_arg)
-  
+def prime?(prime)
+    prime = true
+    for r in 2..Math.sqrt(self).to_i
+      if (self % r == 0)
+        prime = false
+        break
+      end
+    end
+    return prime
+  end
 end
